05aca4954e9a5b4b8cf4f7ed6cec4ea2e08c001e
Tim Lucas <t@toolmantim.com>
2015-03-24 11:46:35 +1100

    First commit :tada:

---
+++ web/index.php
@@ -0,0 +1,25 @@
+<?php
+require('../vendor/autoload.php');
+
+// Logger setup
+$log_handler = new Monolog\Handler\StreamHandler("php://stdout", Logger::WARNING);
+$log_handler->setFormatter(new Monolog\Formatter\LineFormatter("%message%"));
+$logger = new Monolog\Logger("log");
+$logger->pushHandler($log_hander);
+
+// Fetch the expected config environment variables
+$lifx_access_token = getenv('LIFX_ACCESS_TOKEN');
+$bulb_selector     = getenv('BULB_SELECTOR');
+$project_name      = getenv('PROJECT_NAME');
+$branch_name       = getenv('BRANCH_NAME');
+$webhook_token     = getenv('WEBHOOK_TOKEN');
+
+// Process the webhook
+$request_event = $_SERVER['HTTP_X_BUILDKITE_EVENT'];
+$request_token = $_SERVER['HTTP_X_BUILDKITE_TOKEN'];
+$request_body = http_get_request_body();
+
+$logger->addInfo("Webhook event: {$request_event}");
+$logger->addInfo("Webhook token: {$request_token}");
+$logger->addInfo("Webhook request: {$request_body}");
+?>
